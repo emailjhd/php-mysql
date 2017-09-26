04e9115ed2bc211763254c34a3cb4993db9988cf
Slim Garci <slim_garci@hotmail.com>
2017-09-26 09:09:13 +0000

    Commiting good working files

--- dropdown.php
+++ dropdown.php
@@ -11,11 +11,7 @@ mysql_connect($servername,$username,$password);
 mysql_select_db($dbname) or die( "Unable to select database");
 
 
-//$fld_id = "$_POST[fld_id]";
-// $fld_trader = "$_POST[fld_trader]";
 
-// $query="select distinct fld_trader from tbl_trader where fld_trader in ($fld_trader) order by fld_trader;";
-$query="select fld_id, fld_company_name from tbl_company where fld_trader_lnk=$fld_id order by fld_id;";
 $query="select fld_id, fld_trader from tbl_trader order by fld_trader;";
 
 $result=mysql_query($query);
